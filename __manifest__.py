4dbcfc84444d97fed9ff5c92214696d7e7598db3
Ferdiansah <ferdiayana@gmail.com>
2019-11-27 14:39:34 +0700

    mantap

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Aplikasi e-Office',
+    'version': '1.0',
+    'depends': ['base','mail'],
+    'author'  :'vitraining.com',
+    'category': 'Other',
+    "website":"https://vitraining.com",
+    'description': "Aplikasi E-Office",
+    'data': [
+        'views/eo_menu.xml',
+        'views/doc.xml',
+        'views/doc_template.xml',
+        'data/doc_template.xml',
+        'data/doc_type.xml',
+        'data/ir_sequence.xml',
+        'security/ir.model.access.csv',
+        'security/record_rules.xml',
+    ],
+}
+
+#'reports/doc_report_menu.xml','reports/doc.xml',  'data/ir_sequence.xml', 'data/doc_type.xml','data/doc_template.xml',    	
